6c5c8f4e853194e18aee4877cac359c40b64339d
survior <hpy253215039@163.com>
2019-07-05 19:14:40 +0800

    Update process-jdk-tar-gz.sh

--- process-jdk-tar-gz.sh
+++ process-jdk-tar-gz.sh
@@ -1,7 +1,9 @@
 rm -rf jdk.tar.gz
 
+#jdk-8u181-linux-x64.tar.gz需要从官网下载
 tar -xzvf jdk-8u181-linux-x64.tar.gz
 
+#image中压入的jdk的路径是/app/3rd/jdk/default，这里的jdk/default是做路径。
 mkdir -p jdk/default
 mv jdk1.8.0_181/* jdk/default
 chmod -R 755 jdk
